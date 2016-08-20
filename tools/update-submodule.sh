b625cdea52bb45ce8a39cc57ea874f654271e4ee
Yifan Gao <git@gaoyifan.com>
2016-08-21 01:07:48 +0800

    auto commit after update

--- tools/update-submodule.sh
+++ tools/update-submodule.sh
@@ -8,7 +8,10 @@ fi
 cd smartentry
 git fetch origin $branch
 git reset --hard origin/$branch
+commit_id=$(git rev-parse HEAD)
+cd $basedir
 find $basedir -mindepth 2 -maxdepth 2 -type d -not -path '*/.git/*' -not -path '*/smartentry/*' |
 while read dir; do
     cp $basedir/smartentry/smartentry.sh $dir;
 done
+git commit -C $commit_id
