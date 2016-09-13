1af9d0f71419adafe5ff3469a08f8347fe7eac25
Yifan Gao <git@yfgao.com>
2016-09-13 17:58:06 +0800

    auto tag

--- tools/update-submodule.sh
+++ tools/update-submodule.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 basedir=$(realpath $1)
-branch=$(git symbolic-ref --short HEAD)
+tag=${2:-"master"}
 cd $basedir
 if [[ ! -e smartentry ]]; then
     git clone https://github.com/gaoyifan/smartentry.git smartentry
 fi
 cd smartentry
-git fetch origin $branch
-git reset --hard origin/$branch
-commit_id=$(git rev-parse HEAD)
+git fetch origin $tag
+commit_id=$(git rev-list -n 1 $tag)
+git reset --hard $commit_id
 cd $basedir
 find $basedir -mindepth 2 -maxdepth 2 -type d -not -path '*/.git/*' -not -path '*/smartentry/*' |
 while read dir; do
     cp $basedir/smartentry/smartentry.sh $dir;
 done
 git commit -a -m $commit_id
+git tag $tag
